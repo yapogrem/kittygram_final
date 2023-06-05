524465c23dc9f4cf5110779e3dd27ad820c06595
Pogrem <0961@list.ru>
2023-06-05 11:20:27 +0300

    docker 3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -101,7 +101,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
