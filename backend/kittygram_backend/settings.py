931bcc58f864dfeefbc4acdcc8654c43139ff013
Pogrem <0961@list.ru>
2023-06-06 17:50:00 +0300

    static & media

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'key')
 
 DEBUG = bool(distutils.util.strtobool(os.getenv('DEBUG', 'false')))
 
