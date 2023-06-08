7ce6418c82d4b3726a0e754623a1c665cc58a791
pogrem <pogrem@gmail.com>
2023-06-08 18:17:18 +0300

    nginx.conf

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # DEBUG = bool(distutils.util.strtobool(os.getenv('DEBUG'))) не пропускает pytest:
 # File "/home/pogrem/Dev/kittygram_final/backend/kittygram_backend/settings.py", line 9, in <module>
